95dc4d647bd3c0ba8d33f765dd384520c00b6fc6
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-01-05 20:39:18 +0000

    Add favicon, and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
 })
 
@@ -23,6 +29,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer hnas been processed
  */
 function runGame(gameType) {
+    let answerBox = document.getElementById("answer-box");
+    answerBox.value = "";
+    answerBox.focus();
 
     //Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
